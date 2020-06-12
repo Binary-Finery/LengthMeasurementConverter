43de269a897c29efe108be9aa6a760710ea19425
Dean Spencer <deanspencer007@gmail.com>
2020-06-12 20:02:49 +0100

    mods 'n stuff

--- app/src/main/java/com/spencerstudios/lengthmeasurementconverter/Const.kt
+++ app/src/main/java/com/spencerstudios/lengthmeasurementconverter/Const.kt
@@ -3,8 +3,3 @@ package com.spencerstudios.lengthmeasurementconverter
 const val CM = 10.0
 const val INCH = 25.4
 const val FOOT = 304.8
-
-const val EDIT_TEXT_MM = 0
-const val EDIT_TEXT_CM = 1
-const val EDIT_TEXT_INCH = 2
-const val EDIT_TEXT_FOOT = 3
--- app/src/main/java/com/spencerstudios/lengthmeasurementconverter/MainActivity.kt
+++ app/src/main/java/com/spencerstudios/lengthmeasurementconverter/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.spencerstudios.lengthmeasurementconverter
 
 import android.os.Bundle
-import android.view.inputmethod.EditorInfo
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnFocusChangeListener, TextWatcher {
 
     private lateinit var arrayOfEditTexts: Array<EditText>
 
@@ -18,17 +21,123 @@ class MainActivity : AppCompatActivity() {
 
         arrayOfEditTexts = arrayOf(etMm, etCm, etInch, etFt)
 
-        (0 until arrayOfEditTexts.size).forEach { i ->
-            arrayOfEditTexts[i].onFocusChangeListener = FocusListener(arrayOfEditTexts)
-            arrayOfEditTexts[i].setOnEditorActionListener { _, id, _ ->
-                when (id) {
-                    EditorInfo.IME_ACTION_DONE -> {
-                        calc(arrayOfEditTexts[i], arrayOfEditTexts)
-                        true
-                    }
-                    else -> false
-                }
+        0.until(arrayOfEditTexts.size).forEach { i ->
+            arrayOfEditTexts[i].onFocusChangeListener = this
+        }
+        btnReset.setOnClickListener { reset() }
+    }
+
+    override fun afterTextChanged(editable: Editable?) {
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+        var input = "$s"
+        if (input == "." || input == ",")
+            input = "0"
+
+        if (etMm.hasFocus()) {
+            if ("$s".isNotEmpty()) {
+                val v = input.toDouble()
+                etCm.setText(formatValue(v / CM, etCm))
+                etInch.setText(formatValue(v / INCH,etInch))
+                etFt.setText(formatValue(v / FOOT,etFt))
+            } else {
+                etCm.setText("")
+                etInch.setText("")
+                etFt.setText("")
             }
         }
+        if (etCm.hasFocus()) {
+            if ("$s".isNotEmpty()) {
+                val v = input.toDouble()
+                val mm = v * CM
+                etMm.setText(formatValue(mm, etMm))
+                etInch.setText(formatValue(mm / INCH, etInch))
+                etFt.setText(formatValue(mm / FOOT, etFt))
+            } else {
+                etMm.setText("")
+                etInch.setText("")
+                etFt.setText("")
+            }
+        }
+
+        if (etInch.hasFocus()) {
+            if ("$s".isNotEmpty()) {
+                val v = input.toDouble()
+                val mm = v * INCH
+                etMm.setText(formatValue(mm, etMm))
+                etCm.setText(formatValue(mm / CM, etCm))
+                etFt.setText(formatValue(mm / FOOT, etFt))
+            } else {
+                etMm.setText("")
+                etCm.setText("")
+                etFt.setText("")
+            }
+        }
+        if (etFt.hasFocus()) {
+            if ("$s".isNotEmpty()) {
+                val v = input.toDouble()
+                val mm = v * FOOT
+                etMm.setText(formatValue(mm, etMm))
+                etCm.setText(formatValue(mm / CM, etCm))
+                etInch.setText(formatValue(mm / INCH, etInch))
+            } else {
+                etMm.setText("")
+                etCm.setText("")
+                etInch.setText("")
+            }
+        }
+    }
+
+    override fun onFocusChange(v: View?, hasFocus: Boolean) {
+        if (v?.hasFocus()!!) {
+            arrayOfEditTexts.forEach { i ->
+                if (v as EditText == i) {
+                    i.addTextChangedListener(this)
+                } else i.removeTextChangedListener(this)
+            }
+        }
+    }
+
+    private fun formatValue(value: Double, et : EditText): String {
+        val stringValue = String.format(Locale.getDefault(), "%f", value)
+
+
+        val doub = stringValue.toDouble()
+
+        if(doub >= Double.MAX_VALUE){
+            et.error = "number too big!"
+        }
+
+        if (doub % 1 > 0.0) {
+            return if (stringValue[stringValue.length - 1] == '0') {
+                removeTrailingZeros(stringValue)
+            } else stringValue
+        }
+        return "${doub.toLong()}"
+    }
+
+    private fun removeTrailingZeros(n: String): String {
+        var idx = 1
+        for (i in n.length - 1 downTo 0) {
+            if (n[i] == '0')
+                idx++
+            else break
+        }
+        return n.substring(0..n.length - idx)
+    }
+
+    private fun reset() {
+        var x = 0
+        for (i in 0 until arrayOfEditTexts.size) {
+            arrayOfEditTexts[i].removeTextChangedListener(this)
+            arrayOfEditTexts[i].setText("")
+            if (arrayOfEditTexts[i].hasFocus()) x = i
+        }
+        arrayOfEditTexts[x].addTextChangedListener(this)
     }
 }
